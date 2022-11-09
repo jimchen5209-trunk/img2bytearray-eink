16bd3b1cdb6dbfa43cbc458326df3f597e6ab2e9
jimchen5209 <jimchen5209@gmail.com>
2022-11-09 11:52:01 +0800

    feat: change format to eink

--- img2bytearray.py
+++ img2bytearray.py
@@ -4,15 +4,19 @@ import sys
 
 if len(sys.argv) > 1:
     path_to_image = str(sys.argv[1])
-    x = int(sys.argv[2])
-    y = int(sys.argv[3])
+    width = int(sys.argv[2])
+    height = int(sys.argv[3])
 
     im = Image.open(path_to_image).convert('1')
-    im_resize = im.resize((x,y))
-    buf = BytesIO()
-    im_resize.save(buf, 'ppm')
-    byte_im = buf.getvalue()
-    temp = len(str(x) + ' ' + str(y)) + 4
-    print(byte_im[temp::])
+    im_resize = im.resize((width,height))
+
+    buf = [0xFF] * (int(width/8) * height)
+    pixels = im_resize.load()
+    for y in range(height):
+        for x in range(width):
+            # Set the bits for the column of pixels at the current position.
+            if pixels[x, y] == 0:
+                buf[int((x + y * width) / 8)] &= ~(0x80 >> (x % 8))
+    print(bytes(buf))
 else:
     print("please specify the location of image i.e img2bytearray.py /path/to/image width heigh")
