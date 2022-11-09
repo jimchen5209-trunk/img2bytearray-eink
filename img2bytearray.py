b93aa3a6891d064d0f545c7760f971e68602300a
jimchen5209 <jimchen5209@gmail.com>
2022-11-09 13:22:13 +0800

    refactor: convert to function

--- img2bytearray.py
+++ img2bytearray.py
@@ -1,13 +1,7 @@
-from io import BytesIO
 from PIL import Image
 import sys
 
-if len(sys.argv) > 1:
-    path_to_image = str(sys.argv[1])
-    width = int(sys.argv[2])
-    height = int(sys.argv[3])
-
-    im = Image.open(path_to_image).convert('1')
+def convert_to_bytearray(image: Image, width: int, height: int):
     im_resize = im.resize((width,height))
 
     buf = [0xFF] * (int(width/8) * height)
@@ -17,6 +11,15 @@ if len(sys.argv) > 1:
             # Set the bits for the column of pixels at the current position.
             if pixels[x, y] == 0:
                 buf[int((x + y * width) / 8)] &= ~(0x80 >> (x % 8))
-    print(bytes(buf))
-else:
-    print("please specify the location of image i.e img2bytearray.py /path/to/image width heigh")
+    return bytes(buf)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        path_to_image = str(sys.argv[1])
+        width = int(sys.argv[2])
+        height = int(sys.argv[3])
+
+        im = Image.open(path_to_image).convert('1')
+        print(convert_to_bytearray(im,width,height))
+    else:
+        print("please specify the location of image i.e img2bytearray.py /path/to/image width heigh")
